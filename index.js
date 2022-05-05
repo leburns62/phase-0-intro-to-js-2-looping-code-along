c8434ed69f96b3c4066c36e4e638c1bd336fbd80
Levi Hutchinson Burns <leburns62@gmail.com>
2022-05-04 20:53:33 -0400

    commit tree 7c8883343493d07660a0891293011da14be5a43e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let message = []
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return message;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
